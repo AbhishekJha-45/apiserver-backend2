361b2a1d2fce6d9a12a510d887e62ff9d4cbb69f
Your GitHub Username <krjha36555@gmail.com>
2023-08-04 10:40:02 +0000

    latest commit at 4 pm

--- main.js
+++ main.js
@@ -2,60 +2,44 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const mongoSanitize = require("express-mongo-sanitize"); // For sanitizing MongoDB queries
+const helmet = require("helmet"); // Adds security-related HTTP headers
+const xss = require("xss-clean"); // Prevents cross-site scripting attacks
+const path = require("path");
 const cors = require("cors");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
-const routes = require("./routes/routes");
 
+const app = express();
 const PORT = process.env.PORT || 4000;
 
-<<<<<<< HEAD
+// Set up middleware for enhanced security
+app.use(helmet());
+app.use(xss());
 app.use(cors());
-=======
-const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(mongoSanitize());
 
-// Enable CORS with specific whitelist domains
-const allowedOrigins = ["http://example.com", "http://localhost:3000"]; // Add your allowed domains here
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-}));
-
-app.use(helmet()); // Enable various security headers
-
-// Implement rate limiting to prevent abuse or DoS attacks
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // Limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
-
->>>>>>> e7eaa655aa52944f48a0d0c1dda29d0e8a410b2c
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
+db.on("error", (error) => console.error("MongoDB connection error:", error));
 db.once("open", () => console.log("---Connected to Database---"));
 
-// Middlewares
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// Securely store session secret
+if (!process.env.SECRET_KEY) {
+  console.error("No session secret found in environment.");
+  process.exit(1);
+}
 
 app.use(
   session({
-<<<<<<< HEAD
     secret: process.env.SECRET_KEY,
-    saveUninitialized: true,
-    resave: true,
-=======
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false,
->>>>>>> e7eaa655aa52944f48a0d0c1dda29d0e8a410b2c
+    saveUninitialized: false, // Only create session for users with data
+    resave: false, // Do not save session if unmodified
+    cookie: {
+      secure: true, // Use only over HTTPS
+      httpOnly: true, // Prevents JavaScript access to cookies
+      sameSite: "strict", // Mitigates cross-site request forgery (CSRF) attacks
+    },
   })
 );
 
@@ -68,19 +52,19 @@ app.use((req, res, next) => {
 // Set template engine
 app.set("view engine", "ejs");
 
-// Using the router from routes.js
-app.use("/", routes);
+// Route prefix
+app.use("/", require("./routes/routes"));
 
-// Setting static path for css and js files
+// Setting static paths
 const staticpath = path.join(__dirname, "../public");
 app.use(express.static(staticpath));
 
-// Setting static images path
 const staticImagePath = path.join(__dirname, "uploads");
 app.use("/images", express.static(staticImagePath));
 
+// Implement authentication and authorization for routes as needed
 
-// getting requests from the clients
+// Starting the server
 app.listen(PORT, () => {
-  console.log(`server started at http://localhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
